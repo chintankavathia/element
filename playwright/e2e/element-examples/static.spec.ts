bd6da7b38732c2f4e282cbbfd0519e880e1f5fc4
chintankavathia <chintankumar.kavathia@siemens.com>
2025-08-21 11:39:24 +0530

    ci: add buttons tests

---
+++ playwright/e2e/element-examples/static.spec.ts
@@ -0,0 +1,8 @@
+/**
+ * Copyright (c) Siemens 2016 - 2025
+ * SPDX-License-Identifier: MIT
+ */
+import { test } from '../../support/test-helpers';
+
+test('badges/badges', ({ si }) => si.static());
+test('buttons/buttons', ({ si }) => si.static());
